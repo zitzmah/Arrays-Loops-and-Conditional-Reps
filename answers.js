3a53998fe87c42b36c1ed3707fde3f333f956761
zitzmah <haleyzitzmann@gmail.com>
2023-11-05 18:20:19 -0700

    Get Even answered

--- answers.js
+++ answers.js
@@ -1,6 +1,7 @@
 ////////////////////////////////
 // Easy Going
 ////////////////////////////////
+//Write a for loop that will log the numbers 1 through 20.
 for(i=1; i < 21; i ++){
     console.log(i);
 }
@@ -8,7 +9,12 @@ for(i=1; i < 21; i ++){
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
-
+//Write a for loop that will log only the even numbers in 0 through 200.
+for(i =0; i <= 200; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
 
 ////////////////////////////////
 // Fizz Buzz
